91a55ed324be6dfc0aafbdaad0ad5a102cbd953e
ricgellman <ricaprado@yahoo.com>
2024-03-30 08:36:41 -0300

    Update Main.ts

--- Main.ts
+++ Main.ts
@@ -4,5 +4,5 @@ const dviScreen = new DVIScreen('Using DVI Connectivity');
 const adapter = new AdapterUSB();
 
 // Displaying content
-adapter.display(hdmiScreen);
-adapter.display(dviScreen);
+adapter.connect(hdmiScreen);
+adapter.connect(dviScreen);
